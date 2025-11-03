88fe458dbe8d234e44edb29c53787ef9dc842738
yvanddniyo <nyvan80076@gmail.com>
2025-11-03 22:12:31 +0200

    initial commit

---
+++ HelloWorld/hello.py
@@ -0,0 +1,29 @@
+print("Hello World 🙋")
+print("*" * 10)
+x = 10
+y = 12
+unit_price = 14
+first = "Yvan"
+last = "Dexter"
+full = first + " " + last
+print("full", full)
+print("F", f"{first} {last}")
+v = input("x: ")
+z = int(v) + 3
+print("z", f"x: {x}, sum: {z}")
+
+course = " programming python "
+
+# course.
+
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.find("py"))
+print(course.replace("py", "j"))
+print("title", "pro" in course)
+
+print(len(course))
+print(course[-1])
+print(course[:3])
