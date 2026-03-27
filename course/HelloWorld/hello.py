9cd49cc77ab01a5e307ca4abdd5998a0948da195
yvanddniyo <nyvan80076@gmail.com>
2026-03-27 14:38:07 +0200

    Learning

---
+++ course/HelloWorld/hello.py
@@ -0,0 +1,40 @@
+# print("Hello World 🙋")
+# print("*" * 10)
+# x = 10
+# y = 12
+# unit_price = 14
+# first = "Yvan"
+# last = "Dexter"
+# full = first + " " + last
+# print("full", full)
+# print("F", f"{first} {last}")
+# v = input("x: ")
+# z = int(v) + 3
+# print("z", f"x: {x}, sum: {z}")
+
+# course = " programming python "
+
+# # course.
+
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+# print(course.strip())
+# print(course.find("py"))
+# print(course.replace("py", "j"))
+# print("title", "pro" in course)
+
+# print(len(course))
+# print(course[-1])
+# print(course[:3])
+
+
+# Tuplese
+
+names = ("Roger", "Syd", "Baeu")
+
+names[-1]
+names.index("Roger")
+len(names)
+print("Roger" in names)
+print(sorted(names))
