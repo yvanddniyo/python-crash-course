58436b31f53c9d7842a23e0379e2d133e4c2d27c
yvanddniyo <nyvan80076@gmail.com>
2026-03-27 14:42:07 +0200

    working on the python

---
+++ course/Project/paper_lock_scrissor.py
@@ -0,0 +1,36 @@
+import random
+
+
+def get_choices():
+    player_rock = input('Enter a choice(rock, paper, scissors): ')
+    options = ['rock', 'paper', 'scissors']
+
+    computer_choice = random.choice(options)
+    choices = {"player": player_rock, "computer": computer_choice}
+    return choices
+
+
+def check_win(player, computer):
+    print(f"You chose {player}, Computer chose {computer}.")
+    if player == computer:
+        return "It's a tie"
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock smash scissors! You win"
+        else:
+            return "Paper covers rock! you lose"
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper covers rock! You win"
+        else:
+            return "Scissors cuts paper! You lose"
+    elif player == "scissors":
+        if computer == "paper":
+            return "Scissors cut paper! You win"
+    else:
+        return "Rock smashes scissors! You lose"
+
+
+choices = get_choices()
+result = check_win(choices["player"], choices["computer"])
+print("result", result)
